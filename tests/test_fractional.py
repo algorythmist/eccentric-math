363a11acc8d2974d8ba16edde61e6a6ef7c4db70
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-06-21 20:38:21 -0700

    continued fractions

--- tests/test_fractional.py
+++ tests/test_fractional.py
@@ -1,21 +1,18 @@
-import unittest
-
 from fractional import *
 
 
-class FractionalTestCase(unittest.TestCase):
+def test_linear_polynomial():
+    """
+    Verify that D^a x = ax
+    :return:
+    """
+    p = Polynomial([0, 1])
+    assert Polynomial([0, 0.5]) == fractional_derivative(p, 0.5)
+    assert Polynomial([0, 0.2]) == fractional_derivative(p, 0.2)
 
-    def test_linear_polynomial(self):
-        """
-        Verify that D^a x = ax
-        :return:
-        """
-        p = Polynomial([0, 1])
-        self.assertEqual(Polynomial([0, 0.5]), fractional_derivative(p, 0.5))
-        self.assertEqual(Polynomial([0, 0.2]), fractional_derivative(p, 0.2))
 
-    def test_quadratic_polynomial(self):
-        p = Polynomial([0, 0, 0.5])
-        self.assertEqual(Polynomial([0, 0.5]), fractional_derivative(p, 1.5))
-        p = Polynomial([1, 1, 1])
-        self.assertEqual(Polynomial([0.375, 0.25, 0.5]), fractional_derivative(p, 0.5))
+def test_quadratic_polynomial():
+    p = Polynomial([0, 0, 0.5])
+    assert Polynomial([0, 0.5]) == fractional_derivative(p, 1.5)
+    p = Polynomial([1, 1, 1])
+    assert Polynomial([0.375, 0.25, 0.5]) == fractional_derivative(p, 0.5)
